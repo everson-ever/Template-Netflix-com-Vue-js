2ac9b759022af7dc3c53fc192621ad4aa0419c71
Everson Silva <eversonsilva9799@gmail.com>
2019-01-07 11:05:52 -0300

    Tudo funcionando menos e visualização da lista de filmes que está com bug no scroll

---
+++ src/componentes/teste.vue
@@ -0,0 +1,18 @@
+<template>
+    <h1> {{ titulo }} </h1>
+</template>
+
+<script>
+export default {
+    name: 'teste',
+    props: ['titulo']
+}
+</script>
+
+<style scoped>
+    h1{
+        color: red;
+    }
+</style>
+
+
